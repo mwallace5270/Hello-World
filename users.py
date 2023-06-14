c783e00652db1197d4d76f2c6e235286b1a98c9d
Micah <micahwallace00@gmail.com>
2023-06-14 13:36:49 -0400

    fix variables

--- users.py
+++ users.py
@@ -3,7 +3,7 @@ import json
 open_users = open(users.json) 
 data = json.load(open_users) 
 
-for i in data ['first_name']: 
-    print(i) 
+for name in data ['first_name']: 
+    print(name) 
 
 open_users.close()  

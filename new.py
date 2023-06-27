bf2f1726376136ff71d39d77fea61aa98b097bd7
Micah <micahwallace00@gmail.com>
2023-06-27 15:35:02 -0400

    trying to get cartopy installed

--- new.py
+++ new.py
@@ -1,3 +1,3 @@
 import xarray
-ds = xarray.open_dataset("C:/Users/micah/Downloads/VNP02MOD.A2023166.0236.002.2023166103035.nc") 
+ds = xarray.open_dataset("C:/Users/micah/Downloads/VNP02MOD.A2020166.0236.002.2021127081425.nc") 
 print(ds) 
